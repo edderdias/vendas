7933f86dd28bcbdcf169b1f841e94dd646d68f41
Eder Dias <edderdias@gmail.com>
2026-01-30 22:37:28 -0300

    Comecando a desenvolver o sistema - estrutura montada

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
-import { StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import styled from 'styled-components/native';
 
-import Example from './Example';
-
-const TextNew = styled.Text`
-  font-size: 24px;
-  color: pink;
-`;
+import Login from './modules/login';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Example text="ABC">Teste 2</Example>
-      <Example text="João" />
-      <Text style={styles.container}>Testando</Text>
-      <TextNew>New Teste</TextNew>
+      <Login />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    color: 'blue',
-  },
-});
 
 export default App;
