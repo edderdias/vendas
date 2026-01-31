abd77a3ed2c7f15d71ec52b767defdb8775d6714
Eder Dias <edderdias@gmail.com>
2026-01-31 16:16:37 -0300

    Criação do input padrão

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,9 +1,15 @@
 import { Text, View } from 'react-native';
 
+import Input from '../../../shared/components/input/input';
+import { ContainerLogin } from '../styles/login.style';
+
 const Login = () => {
   return (
     <View>
-      <Text />
+      <ContainerLogin>
+        <Text>Primeiro App</Text>
+        <Input />
+      </ContainerLogin>
     </View>
   );
 };
