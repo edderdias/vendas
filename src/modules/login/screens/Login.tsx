fb5ef33ea9c51e34778f164adabe7f01c712f4ed
Eder Dias <edderdias@gmail.com>
2026-02-17 16:24:06 -0300

    Criação do button

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,14 +1,19 @@
 import { Text, View } from 'react-native';
 
+import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/input';
 import { ContainerLogin } from '../styles/login.style';
 
 const Login = () => {
+  const handleOnPress = () => {
+    console.log('Clicou');
+  };
   return (
     <View>
       <ContainerLogin>
         <Text>Primeiro App</Text>
         <Input />
+        <Button margin="16px" title="Entrar" onPress={handleOnPress} />
       </ContainerLogin>
     </View>
   );
---
+++ src/shared/components/button/button.style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+interface ButtonConteinerProps {
+  margin?: string;
+}
+
+export const ButtonContainer = styled.TouchableOpacity<ButtonConteinerProps>`
+  width: 100%;
+  height: 48px;
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+  ${(props: ButtonConteinerProps) => (props.margin ? `margin: ${props.margin};` : '')}
+  background-color: red;
+`;
