9c3755aa90f32852b299aab1556e0f6a3ade2e76
Eder Dias <edderdias@gmail.com>
2026-05-25 09:55:18 -0300

    ajustes

--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,6 +1,7 @@
 import { View } from 'react-native';
 
 import Button from '../../../shared/components/button/Button';
+import { Icon } from '../../../shared/components/icon/icon';
 import Input from '../../../shared/components/input/input';
 import Text from '../../../shared/components/text/text';
 import { theme } from '../../../shared/themes/theme';
@@ -13,6 +14,7 @@ const Login = () => {
   return (
     <View>
       <ContainerLogin>
+        <Icon name="home3" />
         <Text>Primeiro App</Text>
         <Input
           errorMessage="Usuário ou senha inválidos"
